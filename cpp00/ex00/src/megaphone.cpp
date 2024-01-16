fdd380e2aa8b10844b3f25ec919312283700a408
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-01-16 16:16:58 +0100

    minor style and Makefile changes

--- cpp00/ex00/src/megaphone.cpp
+++ cpp00/ex00/src/megaphone.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <algorithm>
 
-int	main(int argc, char **argv) {
-	if (argc == 1) {
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	}
-	else {
-		for (int i = 1; i < argc; i++) {
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
 			std::string str = argv[i];
 			transform(str.begin(), str.end(), str.begin(), ::toupper);
 			std::cout << str;
