5f5b0e2b73922cc5e15cb2a7c39ad07a40b3e794
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-01-24 20:29:20 +0100

    deleted illegal function and wrote an alternative solution

--- cpp00/ex00/src/megaphone.cpp
+++ cpp00/ex00/src/megaphone.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
-#include <algorithm>
 
 int	main(int argc, char **argv)
 {
+	std::string	str;
+	
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
 		for (int i = 1; i < argc; i++)
 		{
-			std::string str = argv[i];
-			transform(str.begin(), str.end(), str.begin(), ::toupper);
-			std::cout << str;
+			str = argv[i];
+			for (size_t j = 0; j < str.length(); j++)
+				std::cout << (char) std::toupper(str[j]);
+
+			if (argc > 2 && i < argc - 1)
+				std::cout << " ";
 		}
 	}
+
 	std::cout << std::endl;
+
 	return (0);
 }
