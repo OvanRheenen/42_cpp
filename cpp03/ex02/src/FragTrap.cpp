82e9ff8ed9427b8399a37205fcf97c5fdff582d7
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-02-12 18:43:20 +0100

    FragTrap class finished

---
+++ cpp03/ex02/src/FragTrap.cpp
@@ -0,0 +1,59 @@
+#include "FragTrap.hpp"
+
+//--Con/destructors-----------------------------------------------------------//
+
+FragTrap::FragTrap()
+{
+	this->setName("JanBot");
+	this->setHitPoints(100);
+	this->setEnergyPoints(100);
+	this->setAttackDamage(30);
+	std::cout	<< "Default constructor called, " 
+				<< "FragTrap " << this->getName()
+				<< " created" << std::endl;
+}
+
+FragTrap::FragTrap(const std::string &name)
+{
+	this->setName(name);
+	this->setHitPoints(100);
+	this->setEnergyPoints(100);
+	this->setAttackDamage(30);
+	std::cout	<< "Name construtor called, "
+				<< "FragTrap " << this->getName()
+				<< " created" << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap &other)
+{
+	std::cout	<< "Copy constructor called, " 
+				<< "copy of FragTrap " << other.getName()
+				<< " created" << std::endl;
+	*this = other;
+}
+
+FragTrap::~FragTrap()
+{
+	std::cout << "Destructor called on FragTrap " << this->getName() << std::endl;
+}
+
+FragTrap	&FragTrap::operator=(const FragTrap &other)
+{
+	this->setName(other.getName());
+	this->setHitPoints(other.getHitPoints());
+	this->setEnergyPoints(other.getEnergyPoints());
+	this->setAttackDamage(other.getAttackDamage());
+	std::cout	<< "Copy assignement operator called, " 
+				<< "copy of FragTrap " << other.getName()
+				<< " made" << std::endl;
+	return (*this);
+}
+
+//--Member functions----------------------------------------------------------//
+
+void	FragTrap::highFivesGuys(void)
+{
+	if (actionPossible("FragTrap", "high five"))
+		std::cout	<< "FragTrap " << getName()
+					<< "wants a high five!" << std::endl;
+}
