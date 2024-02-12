0bc0dd98e96e376253e5dbd0128b3a1149cbf0d8
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-02-12 19:06:27 +0100

    files copied from prev exercises

---
+++ cpp03/ex03/include/FragTrap.hpp
@@ -0,0 +1,20 @@
+#ifndef FRAGTRAP_H
+# define FRAGTRAP_H
+
+# include "ClapTrap.hpp"
+
+class	FragTrap : public ClapTrap
+{
+public:
+
+	FragTrap();
+	FragTrap(const std::string &name);
+	FragTrap(const FragTrap &other);
+	~FragTrap();
+	
+	FragTrap	&operator=(const FragTrap &other);
+
+	void	highFivesGuys(void);
+};
+
+#endif
