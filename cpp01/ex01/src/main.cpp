e62b3359e376f7952ed1c31ff671b9a01bb74580
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-02-26 17:09:32 +0100

    input protection

--- cpp01/ex01/src/main.cpp
+++ cpp01/ex01/src/main.cpp
@@ -2,15 +2,15 @@
 
 int	main(void)
 {
-	int	n = 6;
+	int	n = 1;
 	Zombie	*horde;
 
 	horde = zombieHorde(n, "Steve");
+	if (!horde)
+		return (1);
 
 	for (int i = 0; i < n; i++)
-	{
 		horde[i].announce();
-	}
 
 	delete[] horde;
 
