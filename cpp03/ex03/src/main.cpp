26555d369bc8deb013aba9485fecf4147b46d670
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-02-26 15:11:38 +0100

    tests

--- cpp03/ex03/src/main.cpp
+++ cpp03/ex03/src/main.cpp
@@ -7,9 +7,25 @@
 
 int	main(void)
 {
+	std::cout << "DiamondTrap create: " << std::endl;
 	DiamondTrap	jan("jan");
+	std::cout << "Done\n" << std::endl;
 
 	jan.printStats();
 	jan.whoAmI();
-	jan.attack("arbnout");
+	std::cout << std::endl;
+	jan.attack("arnout");
+	std::cout << std::endl;
+	jan.beRepaired(100);
+	jan.takeDamage(150);
+	std::cout << std::endl;
+
+	jan.guardGate();
+	jan.highFivesGuys();
+	std::cout << std::endl;
+
+	jan.setHitPoints(25);
+	jan.setEnergyPoints(30);
+	jan.setAttackDamage(20);
+	jan.printStats();
 }
