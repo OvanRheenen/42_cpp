d304586d5667b08156f30fbf91418bf0e7704d83
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-02-28 15:10:35 +0100

    added first tests

--- cpp04/ex01/src/main.cpp
+++ cpp04/ex01/src/main.cpp
@@ -7,5 +7,45 @@
 
 int	main(void)
 {
+	{ // given tests
+		const Animal* j = new Dog();
+		const Animal* i = new Cat();
 
+		delete j;
+		delete i;
+	}
+	{ // array test
+		const int size = 100;
+		Animal* animals[size];
+
+		for (int i = 0; i < size / 2; i++)
+		{
+			animals[i] = new Dog();
+		}
+
+		for (int i = size / 2; i < size; i++)
+		{
+			animals[i] = new Cat();
+		}
+		
+		for (int i = 0; i < size; i++)
+		{
+			delete animals[i];
+		}
+	}
+	{ // deep copy tests
+		Cat	a;
+		Cat	b(a);
+
+		std::cout << std::endl << "Deep copy:" << std::endl;
+		a.printMemLocBrain();
+		b.printMemLocBrain();
+
+		WrongCat c;
+		WrongCat d(c);
+
+		std::cout << std::endl << "Shallow copy:" << std::endl;
+		c.printMemLocBrain();
+		d.printMemLocBrain();
+	}
 }
