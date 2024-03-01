589564d7edef57e4939826ac92d7c8d48667c68f
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-03-01 13:33:28 +0100

    readability

--- cpp02/ex02/src/Fixed.cpp
+++ cpp02/ex02/src/Fixed.cpp
@@ -2,35 +2,34 @@
 #include <iostream>
 #include <cmath>
 
+//--Con/Destructors-----------------------------------------------------------//
+
 Fixed::Fixed() :
 	_value(0)
 {
 	// std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const int n)
+Fixed::Fixed(const int n) :
+	_value(n << _binPoint)
 {
 	// std::cout << "Int constructor called" << std::endl;
-	_value = n << _binPoint;
 }
 
 Fixed::Fixed(const float f)
 {
 	// std::cout << "Float constructor called" << std::endl;
+
 	_value = (int)f << _binPoint;							// shift integer part past the fixed point at _binPoint 00000000.00101101 -> 00101101.00000000
 	float fraction = f - (int)f;							// get the fractional part of the float by subtracting the int part (42.42 - 42 = 0.42)
 	fraction = fraction * pow(2, _binPoint);				// get the binary value of the fraction by shifting the . to the right by _binPoint places
 	_value = _value | (int)round(fraction);					// add the fractional part in _value using the | operator
 }
 
-Fixed::~Fixed()
-{
-	// std::cout << "Destructor called" << std::endl;
-}
-
 Fixed::Fixed(const Fixed &fixedPointN)
 {
 	// std::cout << "Copy constructor called" << std::endl;
+
 	*this = fixedPointN;
 }
 
@@ -38,9 +37,18 @@ Fixed	&Fixed::operator=(const Fixed &fixedPointN)
 {
 	if (this != &fixedPointN)
 		this->setRawBits(fixedPointN.getRawBits());
+
 	// std::cout << "Copy assignment operator called" << std::endl;
+
 	return (*this);
 }
+
+Fixed::~Fixed()
+{
+	// std::cout << "Destructor called" << std::endl;
+}
+
+//--Member functions----------------------------------------------------------//
 
 bool	Fixed::operator>(const Fixed &fixedPointN) const
 {
@@ -113,26 +121,32 @@ float	Fixed::operator/(const Fixed &fixedPointN) const
 Fixed	&Fixed::operator++(void)
 {
 	this->_value++;
+
 	return (*this);
 }
 
 Fixed	Fixed::operator++(int)
 {
 	Fixed	tmp(*this);
+
 	operator++();
+
 	return (tmp);
 }
 
 Fixed	&Fixed::operator--(void)
 {
 	this->_value--;
+
 	return (*this);
 }
 
 Fixed	Fixed::operator--(int)
 {
 	Fixed	tmp(*this);
+
 	operator--();
+
 	return (tmp);
 }
 
@@ -183,6 +197,7 @@ float	Fixed::toFloat( void ) const
 {
 	float f = _value >> _binPoint; 					// shift integer part past the fixed point
 	float dec = (_value & 255) / pow(2, _binPoint);	// get only the decimal binary value (_value & 255), shift point to the left by _binPoint places
+	
 	return (f + dec);
 }
 
@@ -191,8 +206,11 @@ int	Fixed::toInt( void ) const
 	return (_value >> _binPoint);
 }
 
+//--Other functions-----------------------------------------------------------//
+
 std::ostream	&operator<<(std::ostream &ostream, const Fixed &fixedPointN)
 {
 	ostream << fixedPointN.toFloat();
+
 	return (ostream);
 }
