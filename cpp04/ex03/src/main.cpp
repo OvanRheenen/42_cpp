83d8caf8fbcf084eae4a080f9213a7734756be65
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-03-05 16:38:31 +0100

    added more tests

--- cpp04/ex03/src/main.cpp
+++ cpp04/ex03/src/main.cpp
@@ -11,22 +11,32 @@ int main()
 	src->learnMateria(new Cure());
 	
 	ICharacter* me = new Character("me");
+	ICharacter* you = new Character("you");
 	
 	AMateria* tmp;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
+	me->equip(tmp);	
+	tmp = src->createMateria("ice");
 	me->equip(tmp);
+	tmp = src->createMateria("ice");
+	me->equip(tmp);	
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	you->equip(tmp);
 
 	ICharacter* bob = new Character("bob");
 
 	me->use(0, *bob);
 	me->use(1, *bob);
 
-	// me->unequip(1);
+	me->unequip(1);
+	you->use(0, *me);
 
 	delete bob;
 	delete me;
+	delete you;
 	delete src;
 
 	return (0);
