08d0987f756f1d447f8ee85bb479fc680f41911c
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-03-06 20:16:54 +0100

    added tests

--- cpp05/ex00/src/main.cpp
+++ cpp05/ex00/src/main.cpp
@@ -3,7 +3,21 @@
 
 int	main(void)
 {
-	std::cout << "test" << std::endl;
+	Bureaucrat	a("a", 1);
+	Bureaucrat	b("b", 150);
+	Bureaucrat	c("c", -1);
+	Bureaucrat	d("d", 151);
+
+	a.incrementGrade();
+	b.decrementGrade();
+
+	std::cout << "before decrementation: " << a << std::endl;
+	a.decrementGrade();
+	std::cout << "after decrementation: " << a << std::endl;
+
+	std::cout << "before incrementation: " << b << std::endl;
+	b.incrementGrade();
+	std::cout << "after incrementation: " << b << std::endl;
 
 	return 0;
 }
