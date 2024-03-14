dbb577809d94b977790c4d1b6c5918dbfd465184
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-03-14 16:10:25 +0100

    class and makefile setup

---
+++ cpp06/ex00/include/ScalarConverter.hpp
@@ -0,0 +1,16 @@
+#ifndef SCALARCONVERTER_H
+# define SCALARCONVERTER_H
+
+class ScalarConverter
+{
+private:
+
+public:
+	ScalarConverter();
+	ScalarConverter(const ScalarConverter &other);
+	ScalarConverter &operator=(const ScalarConverter &other);
+	~ScalarConverter();
+};
+
+
+#endif
---
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -0,0 +1,23 @@
+#include "ScalarConverter.hpp"
+
+//--Con/destructors-----------------------------------------------------------//
+
+ScalarConverter::ScalarConverter()
+{
+
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &other)
+{
+
+}
+
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other)
+{
+
+}
+
+ScalarConverter::~ScalarConverter()
+{
+
+}
