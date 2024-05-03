f81ae6827629f01ff6e20f785ada1c125596eafb
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-05-03 18:50:44 +0200

    made the tests more fool proof and logical

--- cpp05/ex01/src/main.cpp
+++ cpp05/ex01/src/main.cpp
@@ -33,45 +33,58 @@ int	main(void)
 		Bureaucrat	me("me", 25);
 		Bureaucrat	manager("manager", 15);
 		Bureaucrat	boss("boss", 1);
-		Form		form1("contract", 20, 15);
+
+		Form *form1 = nullptr;
+		try
+		{
+			form1 = new Form("contract", 20, 15);
+		}
+		catch (std::exception& e)
+		{
+			std::cout << "Can't create new form: " << e.what() << std::endl;
+			delete form1;
+			return (1);
+		}
 
 		try
 		{
-			form1.beSigned(me);
+			form1->beSigned(me);
 		}
 		catch(const Form::GradeTooLowException& e)
 		{
 			std::cerr	<< me.getName() << " couldn't sign "
-						<< form1.getName() << ": "
+						<< form1->getName() << ": "
 						<< e.what() << std::endl;
 		}
 
-		std::cout << form1 << std::endl;
+		std::cout << *form1 << std::endl;
 
 		try
 		{
-			form1.beSigned(manager);
+			form1->beSigned(manager);
 		}
 		catch(const Form::GradeTooLowException& e)
 		{
 			std::cerr	<< manager.getName() << " couldn't sign "
-						<< form1.getName() << ": "
+						<< form1->getName() << ": "
 						<< e.what() << std::endl;
 		}
 		
-		std::cout << form1 << std::endl;
+		std::cout << *form1 << std::endl;
 
 		// try to sign again
 		try
 		{
-			form1.beSigned(boss);
+			form1->beSigned(boss);
 		}
 		catch(const Form::GradeTooLowException& e)
 		{
 			std::cerr	<< boss.getName() << " couldn't sign "
-						<< form1.getName() << ": "
+						<< form1->getName() << ": "
 						<< e.what() << std::endl;
 		}
+
+		delete form1;
 	}
 	std::cout << "------------------------------------------" << std::endl;
 	{	// Invalid form grades
@@ -83,52 +96,40 @@ int	main(void)
 		{
 			a = new Form("low sign", -2, 50);
 		}
-		catch (const Form::GradeTooLowException& e)
+		catch (std::exception& e)
 		{
 			std::cout << "Can't create new form: " << e.what() << std::endl;
-		}
-		catch (const Form::GradeTooHighException& e)
-		{
-			std::cout << "Can't create new form: " << e.what() << std::endl;
+			delete a;
 		}
 
 		try
 		{
 			a = new Form("high sign", 166, 50);
 		}
-		catch (const Form::GradeTooLowException& e)
+		catch (std::exception& e)
 		{
 			std::cout << "Can't create new form: " << e.what() << std::endl;
-		}
-		catch (const Form::GradeTooHighException& e)
-		{
-			std::cout << "Can't create new form: " << e.what() << std::endl;
+			delete a;
 		}
 
 		try
 		{
 			a = new Form("low exe", 50, -41);
 		}
-		catch (const Form::GradeTooLowException& e)
+		catch (std::exception& e)
 		{
 			std::cout << "Can't create new form: " << e.what() << std::endl;
-		}
-		catch (const Form::GradeTooHighException& e)
-		{
-			std::cout << "Can't create new form: " << e.what() << std::endl;
+			delete a;
 		}
 
 		try
 		{
 			a = new Form("high exe", 50, 431);
 		}
-		catch (const Form::GradeTooLowException& e)
+		catch (std::exception& e)
 		{
 			std::cout << "Can't create new form: " << e.what() << std::endl;
-		}
-		catch (const Form::GradeTooHighException& e)
-		{
-			std::cout << "Can't create new form: " << e.what() << std::endl;
+			delete a;
 		}
 	}
 }
