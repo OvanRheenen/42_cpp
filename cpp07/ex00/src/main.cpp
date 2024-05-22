3d90515e73394f7cbc885f894ac38d1887f40f66
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-05-22 15:53:03 +0200

    cpp07/ex00

---
+++ cpp07/ex00/src/main.cpp
@@ -0,0 +1,40 @@
+#include "whatever.hpp"
+#include <iostream>
+#include <string>
+
+int
+main( void )
+{
+	{	
+		int a = 2;
+		int b = 3;
+
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
+	std::cout << "\nconst tests:\n" << std::endl;
+	{
+		const int a = 2;
+		const int b = 3;
+
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+		const std::string c = "chaine1";
+		const std::string d = "chaine2";
+		
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}	
+	return 0;
+}
