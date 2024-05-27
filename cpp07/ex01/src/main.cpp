bec168814fe5ab8027bca9d41f97927a1307620b
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-05-27 17:12:59 +0200

    added more tests

--- cpp07/ex01/src/main.cpp
+++ cpp07/ex01/src/main.cpp
@@ -3,10 +3,40 @@
 
 int main(void)
 {
-	int array[4] = {1,2,3,4};
+	{	
+		std::cout << "int array test:\n" << std::endl;
+		int array[4] = {1,2,3,4};
 
-	iter(array, 4, &print);
-	iter(array, 4, &increment);
-	std::cout << "after incrementing:" << std::endl;
-	iter(array, 4, &print);
+		std::cout << "before incrementing:" << std::endl;
+		iter(array, 4, &print);
+		iter(array, 4, &increment);
+		std::cout << "after incrementing:" << std::endl;
+		iter(array, 4, &print);
+		std::cout << "\n" << std::endl;
+	}
+	{
+		std::cout << "char array test:\n" << std::endl;
+		char array[4] = {'a','b','c','d'};
+
+		std::cout << "before incrementing:" << std::endl;
+		iter(array, 4, &print);
+		iter(array, 4, &increment);
+		std::cout << "after incrementing:" << std::endl;
+		iter(array, 4, &print);
+		std::cout << "\n" << std::endl;
+	}
+	{
+		std::cout << "string array test:\n" << std::endl;
+		char one[6] = "hello";
+		char two[6] = "world";
+		char three[5] = "it's";
+		char four[3] = "me";
+		char *array[4] = {one,two,three,four};
+
+		std::cout << "before incrementing:" << std::endl;
+		iter(array, 4, &print);
+		iter(array, 4, &increment);
+		std::cout << "after incrementing:" << std::endl;
+		iter(array, 4, &print);
+	}
 }
