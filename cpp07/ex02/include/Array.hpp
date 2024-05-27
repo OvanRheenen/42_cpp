657a31bf4052d85c0d8179e92cd980b56c1d602a
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-05-27 17:28:51 +0200

    more setup

--- cpp07/ex02/include/Array.hpp
+++ cpp07/ex02/include/Array.hpp
@@ -1,1 +1,18 @@
 #pragma once
+
+template <typename T>
+class Array
+{
+private:
+
+public:
+	Array();
+	Array(const unsigned int n);
+	Array(const Array &other);
+	const Array &operator=(const Array &other);
+	~Array();
+
+	const T operator[](const int i);
+
+	size_t size() const;
+};
