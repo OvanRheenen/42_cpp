58a46b0b9510e4d63fb9c9849f06c445c35c449e
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-06-18 15:34:52 +0200

    added some more tests

--- cpp07/ex00/src/main.cpp
+++ cpp07/ex00/src/main.cpp
@@ -22,7 +22,7 @@ main( void )
 		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	}
-	std::cout << "\nconst tests:\n" << std::endl;
+	std::cout << "---------------------\nconst tests:\n" << std::endl;
 	{
 		const int a = 2;
 		const int b = 3;
@@ -35,6 +35,22 @@ main( void )
 		
 		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-	}	
+	}
+	std::cout << "----------------------\nmore types tests:\n" << std::endl;
+	{
+		bool bool_a = true;
+		bool bool_b = false;
+
+		::swap( bool_a, bool_b );
+		std::cout << "bool_a = " << bool_a << ", bool_b = " << bool_b << std::endl;
+
+		double double_a = 3.14;
+		double double_b = 2.718;
+
+		::swap( double_a, double_b );
+		std::cout << "double_a = " << double_a << ", double_b = " << double_b << std::endl;
+		std::cout << "min( double_a, double_b ) = " << ::min( double_a, double_b ) << std::endl;
+		std::cout << "max( double_a, double_b ) = " << ::max( double_a, double_b ) << std::endl;
+	}
 	return 0;
 }
