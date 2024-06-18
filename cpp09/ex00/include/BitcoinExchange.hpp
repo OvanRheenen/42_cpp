e05d11d01d71952d8f085324c6167da98fb47fd9
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-06-18 19:11:16 +0200

    class set up

--- cpp09/ex00/include/BitcoinExchange.hpp
+++ cpp09/ex00/include/BitcoinExchange.hpp
@@ -1,3 +1,24 @@
 #pragma once
 
-#include <array>
+#include <map>
+#include <string>
+
+class BitcoinExchange
+{
+private:
+	std::multimap< std::string, float > _btcExMap;
+	std::multimap< std::string, float > _multRateMap;
+
+	BitcoinExchange();
+
+public:
+	BitcoinExchange(std::multimap< std::string, float > &btcExMap, std::multimap< std::string, float > &multRateMap);
+	BitcoinExchange(const BitcoinExchange &other);
+	BitcoinExchange &operator=(const BitcoinExchange &other);
+	~BitcoinExchange();
+
+	std::multimap< std::string, float > getBtcExMap() const;
+	std::multimap< std::string, float > getMultRateMap() const;
+};
+
+void parsing(char *inputFile);
