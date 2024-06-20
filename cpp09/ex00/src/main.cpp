292a5e5007e39e3a0a8a54945fcec15b173a43d5
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-06-20 15:56:47 +0200

    main done

--- cpp09/ex00/src/main.cpp
+++ cpp09/ex00/src/main.cpp
@@ -6,35 +6,37 @@ int main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		std::cerr << "Please provide one file as argument" << std::endl;
+		std::cerr	<< "Error: provide one argument:\n"
+					<< "./btc <filepath>"
+					<< std::endl;
 		return (1);
 	}
 
-	const char *btcExFile = "./data.csv";
-	BitcoinExchange exchange(parseData(btcExFile));
-	
-	exchange.printBtcExMap();
-	try {
-		exchange.calculate(argv[1]);
-	} catch (std::exception &e) {
+	BitcoinExchange exchange;
+	try
+	{
+		const char *btcExFile = "./data.csv";
+		exchange.setMap(parseData(btcExFile));
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return (1);
+	}
+
+	// //testing
+	// exchange.printBtcExMap();
+	// //testing
+
+	try
+	{
+		parseInput(exchange, argv[1]);
+	}
+	catch(const std::exception& e)
+	{
 		std::cerr << e.what() << std::endl;
 		return (1);
 	}
 
 	return (0);
 }
-
-/**
- * I can also skip parsing the input file and only do that while
- * performing the calculations, so I can still execute the rightly 
- * formatted lines and give an error per line instead of the whole thing
- * OR
- * exit program if input file isn't according to date | value
- * with value >= 0 && <= 1000
- * or give error codes to these pairs in the map and still execute the rest
- * and give an error line by line (more like subject):
- * - Not a positive number
- * - Too large number
- * - Wrong date format
- * - Wrong line format (not date | value)
- */
