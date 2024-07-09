d72b090ceab90984e77fa1f0eefdd6045507d1dd
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-07-09 18:32:47 +0200

    added more functions

--- cpp09/ex02/include/PmergeMe.hpp
+++ cpp09/ex02/include/PmergeMe.hpp
@@ -11,10 +11,17 @@ private:
 	std::vector< int > seqVector;
 	std::list< int > seqList;
 
-	PmergeMe();
 public:
+	PmergeMe();
 	PmergeMe(const std::vector< int > &vector, const std::list< int > &list);
 	PmergeMe(const PmergeMe &other);
 	PmergeMe &operator=(const PmergeMe &other);
 	~PmergeMe();
+
+	void readInput(const int argc, char **input);
+	void sortVector();
+
+	void printOriginal() const;
+	void printVector() const;
+	void printList() const;
 };
