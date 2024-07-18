4fcbd471db654e29e7599b40043aa2ba321ddc78
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-07-18 13:06:21 +0200

    added intra main tests

--- cpp07/ex02/src/main.cpp
+++ cpp07/ex02/src/main.cpp
@@ -57,4 +57,54 @@ int main(void)
 		std::cout << "\narray 3:" << std::endl;
 		arr3.print();
 	}
+	std::cout << "-------------------------------" << std::endl;
+	// #define MAX_VAL 750
+	// {
+	// 	Array<int> numbers(MAX_VAL);
+	// 	int* mirror = new int[MAX_VAL];
+	// 	srand(time(NULL));
+	// 	for (int i = 0; i < MAX_VAL; i++)
+	// 	{
+	// 		const int value = rand();
+	// 		numbers[i] = value;
+	// 		mirror[i] = value;
+	// 	}
+	// 	//SCOPE
+	// 	{
+	// 		Array<int> tmp = numbers;
+	// 		Array<int> test(tmp);
+	// 	}
+
+	// 	for (int i = 0; i < MAX_VAL; i++)
+	// 	{
+	// 		if (mirror[i] != numbers[i])
+	// 		{
+	// 			std::cerr << "didn't save the same value!!" << std::endl;
+	// 			return 1;
+	// 		}
+	// 	}
+	// 	try
+	// 	{
+	// 		numbers[-2] = 0;
+	// 	}
+	// 	catch(const std::exception& e)
+	// 	{
+	// 		std::cerr << e.what() << '\n';
+	// 	}
+	// 	try
+	// 	{
+	// 		numbers[MAX_VAL] = 0;
+	// 	}
+	// 	catch(const std::exception& e)
+	// 	{
+	// 		std::cerr << e.what() << '\n';
+	// 	}
+
+	// 	for (int i = 0; i < MAX_VAL; i++)
+	// 	{
+	// 		numbers[i] = rand();
+	// 	}
+	// 	delete [] mirror;//
+	// 	return 0;
+	// }
 }
