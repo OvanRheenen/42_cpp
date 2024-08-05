5d54276e2f3ecdd654fb39cecd158ef467a24b5a
Oliver van Rheenen <ovan-rhe@student.codam.nl>
2024-08-05 18:46:37 +0200

    readability

--- cpp09/ex02/src/PmergeMe.cpp
+++ cpp09/ex02/src/PmergeMe.cpp
@@ -3,61 +3,40 @@
 
 //--Con/destructors-----------------------------------------------------------//
 
-// VECTOR /////////////////////////
+// VECTOR
 PmergeMeVector::PmergeMeVector(const int argc, char **argv)
 : FordJohnson< std::vector< int >, std::vector< std::pair< int, int > > >(argc, argv) {}
 
 PmergeMeVector::PmergeMeVector() {}
 
-PmergeMeVector::PmergeMeVector(PmergeMeVector &other) 
-{
-	(void)other;
-}
+PmergeMeVector::PmergeMeVector(PmergeMeVector &other) { (void)other; }
 
-PmergeMeVector &PmergeMeVector::operator=(PmergeMeVector &other)
-{
-	(void)other;
-	return (*this);
-}
+PmergeMeVector &PmergeMeVector::operator=(PmergeMeVector &other) { (void)other; return (*this); }
 
 PmergeMeVector::~PmergeMeVector() {}
 
 
-// LIST ///////////////////////
+// LIST
 PmergeMeList::PmergeMeList(const int argc, char **argv)
 : FordJohnson< std::list< int >, std::list< std::pair< int, int > > >(argc, argv) {}
 
 PmergeMeList::PmergeMeList() {}
 
-PmergeMeList::PmergeMeList(PmergeMeList &other) 
-{
-	(void)other;
-}
+PmergeMeList::PmergeMeList(PmergeMeList &other) { (void)other; }
 
-PmergeMeList &PmergeMeList::operator=(PmergeMeList &other)
-{
-	(void)other;
-	return (*this);
-}
+PmergeMeList &PmergeMeList::operator=(PmergeMeList &other) { (void)other; return (*this); }
 
 PmergeMeList::~PmergeMeList() {}
 
-// DEQUE ////////////////////////
+// DEQUE
 PmergeMeDeque::PmergeMeDeque(const int argc, char **argv)
 : FordJohnson< std::deque< int >, std::deque< std::pair< int, int > > >(argc, argv) {}
 
 PmergeMeDeque::PmergeMeDeque() {}
 
-PmergeMeDeque::PmergeMeDeque(PmergeMeDeque &other) 
-{
-	(void)other;
-}
+PmergeMeDeque::PmergeMeDeque(PmergeMeDeque &other) { (void)other; }
 
-PmergeMeDeque &PmergeMeDeque::operator=(PmergeMeDeque &other)
-{
-	(void)other;
-	return (*this);
-}
+PmergeMeDeque &PmergeMeDeque::operator=(PmergeMeDeque &other) { (void)other; return (*this); }
 
 PmergeMeDeque::~PmergeMeDeque() {}
 
